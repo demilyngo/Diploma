d9a44365baf7b92f1053c2aa7d71222d67075c44
demilyngo <79042685+demilyngo@users.noreply.github.com>
2023-06-26 15:15:38 +0500

    Add files via upload

--- src/main/java/com/raspberry/pi4led/models/StationModel.java
+++ src/main/java/com/raspberry/pi4led/models/StationModel.java
@@ -114,7 +114,6 @@ public class StationModel {
                         pin.low();
                         System.out.println(System.currentTimeMillis() - frequencyTimer+30*i);
                         System.out.println("Sent: " + messageBitSet.get(i));
-                        frequencyTimer = System.currentTimeMillis();
                         break;
                     }
                 }
